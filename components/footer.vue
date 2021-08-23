b1a4688d7088ce90c1fd6f9068b7c86331537b96
Giovanni <giovanni.alt1@gmail.com>
2021-08-23 19:02:34 +0200

    Privacy policy e termini e condizioni

--- components/footer.vue
+++ components/footer.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
-      <!-- Footer -->
-      <div class="footer grid flex text-2xl mt-16">
-        <div class="self-center text-red">Footer</div>
+    <!-- Footer -->
+    <div class="footer grid flex text-2xl mt-16">
+      <div class="self-center text-red">Footer</div>
+      <div>
+        <a
+          href="https://www.iubenda.com/privacy-policy/52995361"
+          class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe"
+          title="Privacy Policy "
+          >Privacy Policy</a
+        >
+        <script type="text/javascript">
+          (function (w, d) {
+            var loader = function () {
+              var s = d.createElement("script"),
+                tag = d.getElementsByTagName("script")[0];
+              s.src = "https://cdn.iubenda.com/iubenda.js";
+              tag.parentNode.insertBefore(s, tag);
+            };
+            if (w.addEventListener) {
+              w.addEventListener("load", loader, false);
+            } else if (w.attachEvent) {
+              w.attachEvent("onload", loader);
+            } else {
+              w.onload = loader;
+            }
+          })(window, document);
+        </script>
       </div>
+     
+
+<div>
+  <a href="https://www.privacypolicies.com/live/dc409d01-2d13-4d54-a5e9-5f6f505e6062"> Termini e Condizioni</a>
+ 
+</div>
+     
+    </div>
   </div>
 </template>
 
@@ -20,7 +52,7 @@
   font-size: 2rem;
   text-align: center;*/
   position: relative;
-  background: #ffbbff9f;
+  background: #ffffff;
   bottom: 0;
   width: 100%;
   height: 110px;

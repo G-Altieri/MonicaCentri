42663e74fbde7b2a5d0c05b6cf5485cb5b4fa628
Giovanni Altieri <giovanni.alt1@gmail.com>
2023-02-21 13:33:28 +0100

    landing page

--- pages/landing/index.vue
+++ pages/landing/index.vue
@@ -29,7 +29,7 @@
 
     <!-- Container -->
     <div
-      class="w-full mx-auto flex flex-col items-center"
+      class="w-full mx-auto flex flex-col items-center border-0 lg:border-x-12 border-redSecondary lg:drop-shadow-lg"
       style="max-width: 1024px"
     >
       <!-- Image Header Page -->
@@ -57,9 +57,19 @@
         width="100%"
         height="100%"
       />
+
+<!-- 
+<div style="width: 100%; height: 20px; border-bottom: 5px solid black; text-align: center" class="h-20">
+  <span style="font-size: 40px; padding: 0 10px;" class="text-redSecondary">
+    Section Title 
+  </span>
+</div> -->
+
+
+
     <!-- Content rivoluziona il tuo corpo -->
     <div class="text-center font-bold text-md sm:text-lg ">
-      il protocollo <span class="text-redSecondary">Body Reset</span> é un percorso ideato,<br> studiato e strutturato da <br> Monica Centri
+      Il protocollo <span class="text-redSecondary">Body Reset</span> é un percorso ideato,<br> studiato e strutturato da <br> Monica Centri
     <br><br>
     Attraverso questo <span class="text-redSecondary">viaggio</span>, totalmente <br> <span class="text-redSecondary">personalizzato</span> su di te, ti portiamo al <br>
      <span class="text-redSecondary">benessere</span> psicopatico a 360 gradi. <br> <br> 
@@ -76,7 +86,8 @@
       />
 
      <!-- Cambia te stesso -->
-     <div class="w-full relative mt-16">
+  
+     <div class="w-full relative mt-16 border-y-8 border-redSecondary">
       <img
         class=""
         src="~/assets/img/landing/photo3.png"
@@ -84,10 +95,37 @@
         width="100%"
         height="100%"
       />
-      <div class="text-white text-2xl sm:text-5xl font-bold text-center absolute top-4 left-1/2 transform -translate-x-1/2">Cambia te <br> stesso</div>
+      <div class="text-white text-3xl sm:text-5xl font-bold text-center absolute top-4 left-1/2 transform -translate-x-1/2">Cambia te <br> stesso</div>
       <div class="text-white text-xl sm:text-4xl font-medium text-center absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">PROVA LA SEDUTA <br> BODY REFRESH</div>
       <button class="text-redSecondary text-xl sm:text-4xl font-medium text-center absolute left-1/2 bottom-6 transform -translate-x-1/2 py-4  bg-white w-40 sm:w-80 rounded-full">Prenota ora</button>
      </div>
+
+<!-- Text Risultati Visibili -->
+<div class="text-redTertiary text-3xl sm:text-5xl font-bold text-center my-16">Risultati visibili e <br> misurabili dalla <br> prima seduta</div>
+
+<!-- Section Barre e testo -->
+<div class="font-bold bg-redTertiary w-full py-3 text-white text-center text-xl sm:text-3xl mb-4">PERDITA DI CENTIMETRI</div>
+<div class="font-bold bg-redTertiary w-full py-3 text-white text-center text-xl sm:text-3xl mb-4">CORPO TONIFICATO</div>
+<div class="font-bold bg-redTertiary w-full py-3 text-white text-center text-xl sm:text-3xl mb-4">PELLE RASSODATA</div>
+<div class="font-bold bg-redTertiary w-full py-3 text-white text-center text-xl sm:text-3xl mb-4">RIDUZIONE CELLULITE</div>
+<div class="font-bold bg-redTertiary w-full py-3 text-white text-center text-xl sm:text-3xl mb-4">LEGGEREZZA ED ENERGIA</div>
+    
+
+<!-- Photo di Monica -->
+ <img
+        class="mx-auto mt-12 w-80 "
+        src="~/assets/img/landing/photo4.png"
+        alt="Monica Centri"
+        width="100%"
+        height="100%"
+      />
+
+
+    <!-- Text Scopri il Cambiamento -->
+    <div class="text-center text-redSecondary w-full mx-4 my-6 font-bold text-3xl lg:text-5xl">
+      Scopri il <br> cambiamento
+    </div>
+    <button class="bg-redSecondary text-xl sm:text-4xl font-medium text-center mx-auto py-4 px-8 text-white  rounded-full">Prenota ora</button>
 
 
     </div>
@@ -139,5 +177,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 </style>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,18 @@ module.exports = {
             colors: {
                 'redPrimary': '#AF384F',
                 'redSecondary': '#EC6566',
+                'redTertiary': '#C12E2E',
             },
+        },
+        borderWidth: {
+            DEFAULT: '1px',
+            '0': '0',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '6': '6px',
+            '8': '8px',
+            '12': '12px',
         },
     },
     plugins: [],

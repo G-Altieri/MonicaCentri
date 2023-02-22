222c6512b89e8d7e4def1c572abebef6bd513e69
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2023-02-23 00:14:01 +0100

    landing page design finito ma senza ritocchi

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,19 +4,19 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _2b80598a = () => interopDefault(import('..\\pages\\admin-lp.vue' /* webpackChunkName: "pages/admin-lp" */))
-const _28e456a8 = () => interopDefault(import('..\\pages\\biolifting.vue' /* webpackChunkName: "pages/biolifting" */))
-const _c01359ee = () => interopDefault(import('..\\pages\\card.vue' /* webpackChunkName: "pages/card" */))
-const _8c9e1ee2 = () => interopDefault(import('..\\pages\\documents.vue' /* webpackChunkName: "pages/documents" */))
-const _3feda129 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
-const _1477ded8 = () => interopDefault(import('..\\pages\\landing\\index.vue' /* webpackChunkName: "pages/landing/index" */))
-const _6116dabc = () => interopDefault(import('..\\pages\\laser.vue' /* webpackChunkName: "pages/laser" */))
-const _2bacb1bf = () => interopDefault(import('..\\pages\\lingue.vue' /* webpackChunkName: "pages/lingue" */))
-const _d8e12086 = () => interopDefault(import('..\\pages\\lp.vue' /* webpackChunkName: "pages/lp" */))
-const _15f815a3 = () => interopDefault(import('..\\pages\\mai+xl.vue' /* webpackChunkName: "pages/mai+xl" */))
-const _55dcb287 = () => interopDefault(import('..\\pages\\puliziaevo.vue' /* webpackChunkName: "pages/puliziaevo" */))
-const _c7984c4c = () => interopDefault(import('..\\pages\\qr.vue' /* webpackChunkName: "pages/qr" */))
-const _5c198b8b = () => interopDefault(import('..\\pages\\test.vue' /* webpackChunkName: "pages/test" */))
+const _49c11e46 = () => interopDefault(import('..\\pages\\admin-lp.vue' /* webpackChunkName: "pages/admin-lp" */))
+const _605313ca = () => interopDefault(import('..\\pages\\biolifting.vue' /* webpackChunkName: "pages/biolifting" */))
+const _680bdcaa = () => interopDefault(import('..\\pages\\card.vue' /* webpackChunkName: "pages/card" */))
+const _3675f1a6 = () => interopDefault(import('..\\pages\\documents.vue' /* webpackChunkName: "pages/documents" */))
+const _d73c9272 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _2f6b6cb6 = () => interopDefault(import('..\\pages\\landing\\index.vue' /* webpackChunkName: "pages/landing/index" */))
+const _23e8a840 = () => interopDefault(import('..\\pages\\laser.vue' /* webpackChunkName: "pages/laser" */))
+const _65bb4fe1 = () => interopDefault(import('..\\pages\\lingue.vue' /* webpackChunkName: "pages/lingue" */))
+const _15566edf = () => interopDefault(import('..\\pages\\lp.vue' /* webpackChunkName: "pages/lp" */))
+const _5006b3c5 = () => interopDefault(import('..\\pages\\mai+xl.vue' /* webpackChunkName: "pages/mai+xl" */))
+const _e56920ae = () => interopDefault(import('..\\pages\\puliziaevo.vue' /* webpackChunkName: "pages/puliziaevo" */))
+const _1dfad8fc = () => interopDefault(import('..\\pages\\qr.vue' /* webpackChunkName: "pages/qr" */))
+const _efc56ba6 = () => interopDefault(import('..\\pages\\test.vue' /* webpackChunkName: "pages/test" */))
 
 const emptyFn = () => {}
 
@@ -31,107 +31,107 @@ export const routerOptions = {
 
   routes: [{
     path: "/admin-lp",
-    component: _2b80598a,
+    component: _49c11e46,
     name: "admin-lp___it"
   }, {
     path: "/biolifting",
-    component: _28e456a8,
+    component: _605313ca,
     name: "biolifting___it"
   }, {
     path: "/card",
-    component: _c01359ee,
+    component: _680bdcaa,
     name: "card___it"
   }, {
     path: "/documents",
-    component: _8c9e1ee2,
+    component: _3675f1a6,
     name: "documents___it"
   }, {
     path: "/en",
-    component: _3feda129,
+    component: _d73c9272,
     name: "index___en"
   }, {
     path: "/landing",
-    component: _1477ded8,
+    component: _2f6b6cb6,
     name: "landing___it"
   }, {
     path: "/laser",
-    component: _6116dabc,
+    component: _23e8a840,
     name: "laser___it"
   }, {
     path: "/lingue",
-    component: _2bacb1bf,
+    component: _65bb4fe1,
     name: "lingue___it"
   }, {
     path: "/lp",
-    component: _d8e12086,
+    component: _15566edf,
     name: "lp___it"
   }, {
     path: "/mai%2Bxl",
-    component: _15f815a3,
+    component: _5006b3c5,
     name: "mai+xl___it"
   }, {
     path: "/puliziaevo",
-    component: _55dcb287,
+    component: _e56920ae,
     name: "puliziaevo___it"
   }, {
     path: "/qr",
-    component: _c7984c4c,
+    component: _1dfad8fc,
     name: "qr___it"
   }, {
     path: "/test",
-    component: _5c198b8b,
+    component: _efc56ba6,
     name: "test___it"
   }, {
     path: "/en/admin-lp",
-    component: _2b80598a,
+    component: _49c11e46,
     name: "admin-lp___en"
   }, {
     path: "/en/biolifting",
-    component: _28e456a8,
+    component: _605313ca,
     name: "biolifting___en"
   }, {
     path: "/en/card",
-    component: _c01359ee,
+    component: _680bdcaa,
     name: "card___en"
   }, {
     path: "/en/documents",
-    component: _8c9e1ee2,
+    component: _3675f1a6,
     name: "documents___en"
   }, {
     path: "/en/landing",
-    component: _1477ded8,
+    component: _2f6b6cb6,
     name: "landing___en"
   }, {
     path: "/en/laser",
-    component: _6116dabc,
+    component: _23e8a840,
     name: "laser___en"
   }, {
     path: "/en/lingue",
-    component: _2bacb1bf,
+    component: _65bb4fe1,
     name: "lingue___en"
   }, {
     path: "/en/lp",
-    component: _d8e12086,
+    component: _15566edf,
     name: "lp___en"
   }, {
     path: "/en/mai%2Bxl",
-    component: _15f815a3,
+    component: _5006b3c5,
     name: "mai+xl___en"
   }, {
     path: "/en/puliziaevo",
-    component: _55dcb287,
+    component: _e56920ae,
     name: "puliziaevo___en"
   }, {
     path: "/en/qr",
-    component: _c7984c4c,
+    component: _1dfad8fc,
     name: "qr___en"
   }, {
     path: "/en/test",
-    component: _5c198b8b,
+    component: _efc56ba6,
     name: "test___en"
   }, {
     path: "/",
-    component: _3feda129,
+    component: _d73c9272,
     name: "index___it"
   }],
 
